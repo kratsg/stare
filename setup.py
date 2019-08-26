7d0f32efaaa7a893f2e7ca5ccb41a588fa9320a6
Giordon Stark <kratsg@gmail.com>
2019-08-26 15:57:04 -0700

    flesh out dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup, find_packages
 
 extras_require = {
-    "develop": [""]
+    'develop': [
+        'pyflakes',
+        'pytest',
+        'pytest-cov',
+        'pytest-mock',
+        'coverage',
+        'bumpversion',
+        'pre-commit',
+        'bandit',
+        'black;python_version>="3.6"',  # Black is Python3 only
+        'betamax',  # recording api calls for testing
+        'betamax-serializers',
+        'twine',  # uploading to pypi
+    ],
 }
-extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
+extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
 
 setup(
     name="stare",
@@ -16,7 +29,20 @@ setup(
     author="Giordon Stark",
     author_email="gstark@cern.ch",
     license="BSD 3-clause",
-    install_requires=[""],
-    classifiers=["Development Status :: 1 - Planning"],
+    install_requires=[
+        'requests',  # for all HTTP calls to the API
+        'cachecontrol[filecache]',  # for caching HTTP requests according to spec to local file
+        'click>=6.0',  # for console scripts,
+        'python-jose',  # for id token decoding
+        'attrs',  # for model inflation/deflation
+        'python-dotenv',  # for loading env variables
+    ],
+    classifiers=[
+      "Development Status :: 1 - Planning",
+      "Programming Language :: Python :: 3",
+      "Programming Language :: Python :: 3.7",
+    ],
     extras_require=extras_require,
+    entry_points={'console_scripts': ['stare=stare.commandline:stare']},
+    dependency_links=[],
 )
