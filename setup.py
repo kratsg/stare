da900a1669af8b72fe8fbbf1c83d8d95e412af8e
Giordon Stark <kratsg@gmail.com>
2019-10-17 15:20:54 -0700

    fix up testpypi using the setuptools_scm

--- setup.py
+++ setup.py
@@ -18,9 +18,31 @@ extras_require = {
 }
 extras_require['complete'] = sorted(set(sum(extras_require.values(), [])))
 
+
+def _is_test_pypi():
+    """
+    Determine if the CI environment has TESTPYPI_UPLOAD defined and
+    set to true (c.f. .travis.yml)
+    The use_scm_version kwarg accepts a callable for the local_scheme
+    configuration parameter with argument "version". This can be replaced
+    with a lambda as the desired version structure is {next_version}.dev{distance}
+    c.f. https://github.com/pypa/setuptools_scm/#importing-in-setuppy
+    As the scm versioning is only desired for TestPyPI, for depolyment to PyPI the version
+    controlled through bumpversion is used.
+    """
+    from os import getenv
+
+    return (
+        {'local_scheme': lambda version: ''}
+        if getenv('TESTPYPI_UPLOAD') == 'true'
+        else False
+    )
+
+
 setup(
     name="stare",
     version="0.0.6",
+    use_scm_version=_is_test_pypi(),
     package_dir={"": "src"},
     packages=find_packages(where="src", exclude=["tests"]),
     include_package_data=True,
