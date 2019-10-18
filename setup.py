2c70239d8abf14661816f35f49b98abcaa2ed765
Giordon Stark <kratsg@gmail.com>
2019-10-17 18:17:55 -0700

    Bump version: 0.0.9 → 0.0.10

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def _is_test_pypi():
 
 setup(
     name="stare",
-    version="0.0.9",
+    version="0.0.10",
     use_scm_version=_is_test_pypi(),
     package_dir={"": "src"},
     packages=find_packages(where="src", exclude=["tests"]),
