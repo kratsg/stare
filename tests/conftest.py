6fd79313f396aae3ee9546a14eb314d8f6071401
Giordon Stark <kratsg@gmail.com>
2026-04-18 15:50:06 -0700

    coderabbit suggestion

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,8 +37,8 @@ def pytest_collection_modifyitems(
 
 def _free_port() -> int:
     """Return an available TCP port on localhost."""
-    with socket.socket() as s:
-        s.bind(("", 0))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(("127.0.0.1", 0))
         return int(s.getsockname()[1])
 
 
