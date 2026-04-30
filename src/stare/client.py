1f775e92d154885f3761aeeb1a0b7b2fe7581502
Giordon Stark <kratsg@gmail.com>
2026-04-30 08:28:30 -0400

    add user agent header

--- src/stare/client.py
+++ src/stare/client.py
@@ -10,6 +10,7 @@ import httpx
 from hishel import CacheOptions, SpecificationPolicy, SyncSqliteStorage
 from hishel.httpx import SyncCacheTransport
 from pydantic import TypeAdapter, ValidationError
+from stare._version import version as __version__
 
 from stare.auth import TokenManager
 from stare.dsl import Expression, parse_dsl
@@ -409,6 +410,7 @@ class Glance:
             base_url=self._settings.base_url,
             transport=transport,
             event_hooks={"request": [self._inject_auth]},
+            headers = {'user-agent': f'stare/{__version__}'},
         )
         self.analyses = AnalysisResource(self._http)
         self.papers = PaperResource(self._http)
